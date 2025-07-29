2c18bc4c23e1c828a19bf1eacfb27438b07d1dc8
K-dolkar <kuenzangdolkar868@gmail.com>
2025-07-29 14:50:41 +0600

    my name

--- firstproject.py
+++ firstproject.py
@@ -1,2 +1,7 @@
-print("hello world")
-print("my name")
+a = 2
+b = 3
+sum = a + b
+print("The sum of a and b is; ",sum )
+name = input("Enter your name")
+age = input("Enter your age")
+print("You are",name,"and you are", age,"years old" )
